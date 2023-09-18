a23a522a0a1d5ff1ffcbb4c772a101b9ca58d46d
Nazek-Altayeb <>
2023-09-18 08:37:30 +0000

    add the following functionalities : edit and delete post, create and  edit and delete comment,

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import PostCreateForm from './pages/posts/PostCreateForm';
 import PostPage from './pages/posts/PostPage';
 import PostsPage from './pages/posts/PostsPage';
 import { useCurrentUser } from "./contexts/CurrentUserContext";
+import PostEditForm from './pages/posts/PostEditForm';
 
 
 function App() {
@@ -54,6 +55,8 @@ function App() {
                     <Route exact path='/posts/create' render={() => <PostCreateForm />} />
 
                     <Route exact path="/posts/:id" render={() => <PostPage />} />
+
+                    <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
 
                     <Route render={() => <h1> Page not found !</h1>} />
 
