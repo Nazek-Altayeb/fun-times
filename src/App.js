a49a4ecc7c41ccdd11c94e1d27c4509abf65c025
Nazek-Altayeb <>
2023-09-19 14:54:41 +0000

    create the profile page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PostPage from './pages/posts/PostPage';
 import PostsPage from './pages/posts/PostsPage';
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 import PostEditForm from './pages/posts/PostEditForm';
+import ProfilePage from './pages/profiles/ProfilePage';
 
 
 function App() {
@@ -57,6 +58,8 @@ function App() {
                     <Route exact path="/posts/:id" render={() => <PostPage />} />
 
                     <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
+
+                    <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
 
                     <Route render={() => <h1> Page not found !</h1>} />
 
