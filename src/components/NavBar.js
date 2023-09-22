fbbb6896ca4176262b082856817f07c5e053673c
Nazek-Altayeb <>
2023-09-22 10:09:55 +0000

    change the style of the Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -92,7 +92,7 @@ const NavBar = () => {
             <Container>
                 <NavLink to="/">
                     <Navbar.Brand>
-                        <img src={logo} alt="logo" height="45" />
+                        <img src={logo} alt="logo" height="120" />
                     </Navbar.Brand>
                 </NavLink>
                 {currentUser && addPostIcon}
